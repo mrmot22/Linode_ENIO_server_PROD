4f3a93db2505b2b2bb2e9943fa1acd8df67c0ac6
Igor <igor.sulc@enio.sk>
2025-07-28 14:03:30 +0200

    datepicker

--- routes/DT_region.js
+++ routes/DT_region.js
@@ -68,7 +68,7 @@ router.get('/',checkAuthenticated, async(req, res) => {
     
     }));
 
-    res.render('DT_region', { currentHour: formattedDate, dataJSON: processedData }); // Render index.ejs and pass the data
+    res.render('DT_region', { currentDay: formattedDate, dataJSON: processedData }); // Render index.ejs and pass the data
 
   } catch (err) {
     console.error('Error fetching data:', err);
@@ -81,9 +81,9 @@ router.get('/',checkAuthenticated, async(req, res) => {
 
 router.post('/',checkAuthenticated, async(req,res) => {
 
-  const { direction, currentHour } = req.body;
+  const { direction, currentDay } = req.body;
 
-  let date = new Date(currentHour);
+  let date = new Date(currentDay);
   let new_date = new Date(date);
 
     if (direction.toString() == "backward") {
@@ -131,7 +131,7 @@ router.post('/',checkAuthenticated, async(req,res) => {
       utc_cas: formatUTCToCET(item.utc_cas) ?? null // Ensure a default value
     }));
 
-    res.render('DT_region', { currentHour: formattedDate, dataJSON: processedData }); // Render index.ejs and pass the data
+    res.render('DT_region', { currentDay: formattedDate, dataJSON: processedData }); // Render index.ejs and pass the data
 
   } catch (err) {
 
